0c73a9d578a3366aeb71be72f6fd45ced0a16058
johnlindquist <johnlindquist@gmail.com>
2016-06-20 23:28:53 -0700

    cleaning up the install process, removing global dependencies

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 ### Usage
 - Clone or fork this repository
 - Make sure you have [node.js](https://nodejs.org/) installed
-- run `npm install -g webpack webpack-dev-server typings typescript` to install global dependencies
 - run `npm install` to install dependencies
-- run `npm run typings-install` to install typings
 - run `npm start` to fire up dev server
 - open browser to [`http://localhost:3000`](http://localhost:3000)
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,10 @@ import {Observable} from 'rxjs/Rx';
 import * as $ from 'jquery';
 
 const body = $(document.body);
+const output = $(`<div>Output</div>`);
 
-Observable.interval(1000)
-  .subscribe(count => body.html(`<div>${count}</div>`);
+Observable.from("Observables are lazy Push collections of multiple values".split(" "))
+  .concatMap(word => Observable.of(word).delay(250))
+  .repeat()
+  .subscribe(word => body.html(word));
+
